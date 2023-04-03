27b959c19d52c466e8c82bf136db570cf23bf415
Mihaela Maxim <maxim.mihaela1996@gmail.com>
2023-04-03 11:01:38 +0200

    Create Test.cs

---
+++ Shared/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketHiveSpaceKittens.Shared.Models
+{
+    internal class Test
+    {
+    }
+}
